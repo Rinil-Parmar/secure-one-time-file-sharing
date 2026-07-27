17725b1328f2285f113d02b927a5ab24efeb1449
Rinil-Parmar <parmarrinila2002@gmail.com>
2026-07-26 22:36:53 -0400

    Set up minimal Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
 
+from config import Config
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    @app.route("/")
+    def dashboard():
+        return render_template("dashboard.html")
+
+    return app
